a28e39edfb64d7866b8a281b891cba206a84b0f7
Karsten Schramm <SchramKa@basfad.basf.net>
2020-04-14 08:41:24 +0200

    1.2.0.2

--- SixDegrees/Properties/SixDegrees.AssemblyInfo.cs
+++ SixDegrees/Properties/SixDegrees.AssemblyInfo.cs
@@ -7,5 +7,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+[assembly: AssemblyVersion("1.2.0.2")]
+[assembly: AssemblyFileVersion("1.2.0.2")]
